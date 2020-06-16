f114d5f5abfcf9503611258acf5a04d47f6c8a35
Ryan Carmody <ryancarmody@outlook.com>
2020-06-16 22:24:01 +0930

    adding header and playing with flexbox

---
+++ src/app/make-a-budget-header/make-a-budget-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'make-a-budget-header',
+  templateUrl: './make-a-budget-header.component.html',
+  styleUrls: ['./make-a-budget-header.component.scss']
+})
+export class MakeABudgetHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
